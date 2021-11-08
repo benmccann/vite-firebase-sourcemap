095f087bd0cd91b1d8ecda94f67a67e55842dc07
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-11-08 09:59:57 -0800

    remove workaround for issue fixed in Vite 2.7

--- svelte.config.js
+++ svelte.config.js
@@ -5,11 +5,6 @@ export default {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(),
-		vite: {
-			ssr: {
-				external: ['whatwg-url', 'node-fetch']
-			}
-		}
+		adapter: adapter()
 	}
 }
